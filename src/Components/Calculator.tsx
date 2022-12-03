43ffd4d37f26a5e84a308859de53b7aa27632327
ort01 <ortutova.kristina@gmail.com>
2022-12-03 20:02:38 +0100

    state button

--- src/Components/Calculator.tsx
+++ src/Components/Calculator.tsx
@@ -1,7 +1,16 @@
 import './calculator.scss'
+import { useState } from 'react';
 import Button from '@mui/material/Button';
 
 export function Calculator() {
+
+    const [inputState, setInputState] = useState(false)
+
+    function handleInput() {
+        return setInputState(!inputState)
+    }
+
+
     return (
         <div className="calculatorBody">
             <h3>Vložte informácie</h3>
@@ -9,13 +18,13 @@ export function Calculator() {
                 <label>Hodinová mzda</label>
                 <input
                     name="Money"
-                    placeholder="eur/hod"
+                    placeholder="Eur/Hod"
                     type='number'
                 />
                 <label>Počet odpracovaných hodín za mesiac</label>
                 <input
                     name="Hours"
-                    placeholder="Počet Hodín"
+                    placeholder="Hod"
                     type='number'
                 />
                 <label>Odvody do Sociálnej poisťovne</label>
@@ -32,13 +41,15 @@ export function Calculator() {
                 />
 
             </div>
-            <input
-                name="Checkbox"
-                type='checkbox'
-            />
-            <label id="warranty-limitation">Beriem na vedomie, že prevádzkovateľ tejto webovej stránky na informácie uvedené na tejto stránke neposkytuje žiadne záruky a nenesie žiadnu zodpovednosť za škody akéhokoľvek druhu súvisiace s obsahom na tejto webovej stránke!</label>
-            <Button variant="contained">Potvrdit</Button>
-
-        </div>
+            <div className='confirmation'>
+                <input
+                    name="Checkbox"
+                    type='checkbox'
+                    onClick={handleInput}
+                />
+                <label id="warranty-limitation">Beriem na vedomie, že prevádzkovateľ tejto webovej stránky na informácie uvedené na tejto stránke neposkytuje žiadne záruky a nenesie žiadnu zodpovednosť za škody akéhokoľvek druhu súvisiace s obsahom na tejto webovej stránke!</label>
+            </div>
+            {inputState ? <Button variant="contained">Potvrdiť</Button> : <Button variant="contained" disabled>Potvrdiť</Button>}
+        </div >
     )
 }
--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -3,7 +3,7 @@ import './header.css'
 export function Header() {
   return (
     <div className="header" >
-      <h1>SZČO Calculator</h1>
+      <h1>SZČO Kalkulačka</h1>
     </div>
   )
 }
