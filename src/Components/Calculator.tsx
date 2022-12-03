0871784dcf9a78cff5d5e2ff6a99ed80ee20fac5
petereon <pvyboch1@gmail.com>
2022-12-03 20:55:48 +0100

    feat: adding tooltips

--- src/Components/Calculator.tsx
+++ src/Components/Calculator.tsx
@@ -1,8 +1,23 @@
 import './calculator.scss'
 import { useState } from 'react';
 import Button from '@mui/material/Button';
+import { Tooltip } from '@mui/material';
+import IconButton from '@mui/material/IconButton';
+import HelpIcon from '@mui/icons-material/Help';
+import { ReactJSXElement } from '@emotion/react/types/jsx-namespace';
+import { grey } from '@mui/material/colors';
 
 export function Calculator() {
+
+    const getTooltip = (tooltipText: string): ReactJSXElement => {
+        return (
+            <Tooltip className='tooltip' title={tooltipText}>
+                <IconButton sx={{ color: grey[700] }}>
+                    <HelpIcon />
+                </IconButton>
+            </Tooltip>
+        )
+    }
 
     const [inputState, setInputState] = useState(false)
 
@@ -15,30 +30,34 @@ export function Calculator() {
         <div className="calculatorBody">
             <h3>Vložte informácie</h3>
             <div className='form'>
-                <label>Hodinová mzda</label>
-                <input
+                <label className='left-col'>Hodinová mzda</label>
+                <input className='right-col'
                     name="Money"
-                    placeholder="Eur/Hod"
+                    placeholder="€/hod."
                     type='number'
                 />
-                <label>Počet odpracovaných hodín za mesiac</label>
-                <input
+                {getTooltip("Ponúkaná hodinová mzda bez akýchkoľvek úprav.")}
+                <label className='left-col'>Počet odpracovaných hodín za mesiac:</label>
+                <input className='right-col'
                     name="Hours"
-                    placeholder="Hod"
+                    placeholder="napr. 160"
                     type='number'
                 />
-                <label>Odvody do Sociálnej poisťovne</label>
-                <input
+                {getTooltip("Počet hodín reálne odpracovaných za jeden mesiac.")}
+                <label className='left-col'>Odvody do Sociálnej poisťovne:</label>
+                <input className='right-col'
                     name="Social"
-                    placeholder="Minimálna výška 187,78€"
+                    placeholder="Min. 187,78€ za Mesiac"
                     type='number'
                 />
-                <label>Odvody do Zdravotnej poisťovne</label>
-                <input
+                {getTooltip("Výška odvodov do sociálnej poisťovne. V prvý rok podnikania sa odvody do sociálnej poisťovne neplatia. Výška odvodov určuje aj mieru sociálnej starostlivosti v prípade nezamestnanosti alebo pracovnej neschonosti.")}
+                <label className='left-col'>Odvody do Zdravotnej poisťovne:</label>
+                <input className='right-col'
                     name="Health"
-                    placeholder="Minimálna výška 84,77€"
+                    placeholder="Min. 84,77€ za Mesiac"
                     type='number'
                 />
+                {getTooltip("Výška odvodov do zdravotnej poisťovne.")}
 
             </div>
             <div className='confirmation'>
